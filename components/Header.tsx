2ff776782d1660cf39a1551df637b8a30859ec86
OHMURA Tokiya <tocky.dison@gmail.com>
2022-11-30 01:31:16 +0900

    feat: modify header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,107 @@
-function Header() {
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Example() {
   return (
-    <div>
-      <div>Header</div>
-    </div>
+    <Popover className="relative bg-white">
+      <div className="mx-auto max-w-4xl px-4 sm:px-6">
+        <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <Link href="#">
+              <span className="sr-only">tttocklll</span>
+              <Image
+                height={32}
+                width={120}
+                objectFit="contain"
+                src="/tttocklll.png"
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="-my-2 -mr-2 md:hidden">
+            <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
+          <Popover.Group as="nav" className="hidden space-x-10 md:flex">
+            <Link
+              href="/about"
+              className="text-xl font-medium text-gray-500 hover:text-gray-900"
+            >
+              About
+            </Link>
+            <Link
+              href="/experience"
+              className="text-xl font-medium text-gray-500 hover:text-gray-900"
+            >
+              Experience
+            </Link>
+          </Popover.Group>
+          <div className="hidden space-x-10 justify-end md:flex md:flex-1 lg:w-0">
+            <Link
+              href="#"
+              className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-green-400 px-4 py-2 text-xl font-medium text-white shadow-sm hover:bg-indigo-700"
+            >
+              Contact
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-200 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute top-0 inset-x-0 origin-top-right transform p-2 transition md:hidden"
+        >
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="tttocklll"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 px-5">
+              <div className="grid grid-cols gap-y-4 gap-x-8">
+                <Link
+                  href="/about"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700 border-b pb-2"
+                >
+                  About
+                </Link>
+                <Link
+                  href="/experience"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700 border-b pb-2"
+                >
+                  Experience
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 }
-
-export default Header;

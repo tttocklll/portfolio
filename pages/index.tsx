220d49aba6a0f2c2437ad8cb252d33037197520f
OHMURA Tokiya <tocky.dison@gmail.com>
2022-12-04 02:51:14 +0900

    feat: modify about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,29 +29,54 @@ export default function Home() {
           <h2 className="text-green-600 text-2xl sm:text-4xl after:bg-green-600">
             About Me
           </h2>
-          <div className="w-full flex flex-row border border-gray-100 border-solid rounded-2xl shadow-md">
-            <div className="sm:w-1/4 w-1/3 flex flex-col justify-center items-center my-8 sm:my-16">
+          <div className="w-full flex sm:flex-row flex-col items-center border border-gray-100 border-solid rounded-2xl shadow-md p-4 sm:p-8 space-y-4 sm:space-y-0">
+            <div className="sm:w-1/4 w-full flex flex-row sm:flex-col justify-center items-center">
               <Image
                 src="/profileIcon.png"
                 height={100}
                 width={100}
                 alt="profile icon"
-                className="w-4/5 h-auto sm:mb-8 sm:w-2/3 rounded-full"
+                className="h-auto sm:mb-4 w-auto rounded-full"
               />
-              <div className="sm:flex sm:flex-row justify-around w-4/5 sm:w-2/3 hidden">
-                <Link href="#">
-                  <Twitter className="hover:text-green-600" />
-                </Link>
-                <Link href="#">
-                  <Linkedin className="hover:text-green-600" />
-                </Link>
-                <Link href="#">
-                  <GitHub className="hover:text-green-600" />
-                </Link>
+              <div className="w-full flex flex-col justify-center items-center space-y-2">
+                <div className="flex flex-col justify-center items-center sm:hidden">
+                  <h3 className="text-xl sm:text-2xl font-semibold text-green-600">
+                    大村瞬也
+                  </h3>
+                  <p className="text-gray-500">OHMURA Tokiya</p>
+                </div>
+                <div className="flex flex-row justify-around w-4/5 sm:w-2/3">
+                  <Link href="https://twitter.com/tttockllll">
+                    <Twitter className="hover:text-green-600" />
+                  </Link>
+                  <Link href="https://www.linkedin.com/in/tttocklll/">
+                    <Linkedin className="hover:text-green-600" />
+                  </Link>
+                  <Link href="https://github.com/tttocklll">
+                    <GitHub className="hover:text-green-600" />
+                  </Link>
+                </div>
               </div>
             </div>
 
-            <div className="w-3/4 pl-10 align-middle">hihi</div>
+            <div className="sm:w-3/4 w-full flex flex-col justify-center">
+              <h3 className="hidden sm:flex sm:text-2xl sm:mb-2 font-semibold text-green-600">
+                大村瞬也 | OHMURA Tokiya
+              </h3>
+              <p className="text-base sm:text-lg ">静岡県浜松市出身。</p>
+              <p className="text-base sm:text-lg ">
+                浜松北高卒業後、東京大学文科II類に入学。
+              </p>
+              <p className="text-base sm:text-lg ">
+                アルバイト先の経験からITに興味を持ち、理転し工学部電子情報工学科に進学。
+              </p>
+              <p className="text-base sm:text-lg ">
+                株式会社Stroboでエンジニアインターン。
+              </p>
+              <p className="text-base sm:text-lg ">
+                大学卒業後、日本マイクロソフト株式会社に入社。
+              </p>
+            </div>
           </div>
         </div>
       </main>

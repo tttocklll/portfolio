71c328fdada0ee790bd611b3d8b8f52a1393d411
OHMURA Tokiya <tocky.dison@gmail.com>
2022-12-13 01:38:56 +0900

    fix: bug fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -58,7 +58,7 @@ export default function Header() {
       >
         <Popover.Panel
           focus
-          className="absolute inset-x-0 top-0 origin-top-right p-2 transition md:hidden"
+          className="absolute inset-x-0 top-0 z-50 origin-top-right p-2 transition md:hidden"
         >
           <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black/5">
             <div className="px-5 pt-5 pb-6">

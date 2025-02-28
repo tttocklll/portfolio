91e3c619bf68ae0cd50c929fdec9bf21a07a2d85
tttocklll <tocky.dison@gmail.com>
2025-03-01 03:44:20 +0900

    refactor: redesign homepage and components with refined styling

--- v2/src/components/SocialIcons.jsx
+++ v2/src/components/SocialIcons.jsx
@@ -2,12 +2,12 @@ import { Icon } from "@iconify/react";
 
 export default function SocialIcons() {
   return (
-    <div className="flex space-x-4 justify-center">
+    <div className="flex space-x-6 justify-center">
       <a
         href="https://twitter.com/tttockllll"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-white/60 hover:text-gold transition-colors"
+        className="text-white/40 hover:text-gold/80 transition-colors"
       >
         <Icon icon="mdi:twitter" className="w-5 h-5" />
       </a>
@@ -15,7 +15,7 @@ export default function SocialIcons() {
         href="https://www.linkedin.com/in/tttocklll/"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-white/60 hover:text-gold transition-colors"
+        className="text-white/40 hover:text-gold/80 transition-colors"
       >
         <Icon icon="mdi:linkedin" className="w-5 h-5" />
       </a>
@@ -23,7 +23,7 @@ export default function SocialIcons() {
         href="https://github.com/tttocklll"
         target="_blank"
         rel="noopener noreferrer"
-        className="text-white/60 hover:text-gold transition-colors"
+        className="text-white/40 hover:text-gold/80 transition-colors"
       >
         <Icon icon="mdi:github" className="w-5 h-5" />
       </a>

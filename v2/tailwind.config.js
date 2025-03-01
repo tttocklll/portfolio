344c3791703e9c3dc90a6d0cf7cbbd9bb2a6e3f8
tttocklll <tocky.dison@gmail.com>
2025-03-02 04:27:33 +0900

    chore: style

--- v2/tailwind.config.js
+++ v2/tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        background: '#f4f5f7',
-        text: '#2e2a2c',
-        accent: '#f63f35',
+        background: "#f4f5f7",
+        text: "#2e2a2c",
+        accent: "#f63f35",
       },
     },
   },
   plugins: [],
-}
+};
